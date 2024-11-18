70d9ab4ae50f8fed8fb5d22da61c2dd5922ffb66
vitorgaboardi <51085335+vitorgaboardi@users.noreply.github.com>
2024-11-18 19:00:36 +0000

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -14,3 +14,9 @@ class Rectangle:
             return -1
         else:
             return self.width * self.height
+
+    def get_perimeter(self):
+        if self.width < 0 or self.height < 0:
+            return -1
+        else:
+            return 2 * (self.width + self.height)
